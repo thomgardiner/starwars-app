32eef5e418e5740fc2bc092dfff75b4a3f3ea91f
Thomas Gardiner <thomgardiner@gmail.com>
2018-05-31 13:49:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -76,14 +76,9 @@ app.get("/api/characters/:character", function(req, res) {
   return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
+// Create New Characters
 app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
   var newcharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newcharacter);
